cc06a2a65abb003c293180800da0a6f5cf3d5cbe
nathanmccartney-macbook <nmccartney1231@gmail.com>
2014-05-06 21:03:40 -0400

    added simple validation

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -5,14 +5,25 @@ angular.module('ngUserApp')
     $scope.users = Userserv.users ;
 
     $scope.$on('users.update',function(){
-      console.log($scope.users.length,Userserv.users.length)
+      console.log($scope.users.length,Userserv.users.length);
       $scope.users = Userserv.users ;
       $scope.$apply();
     });
 
-    $scope.addUser = function(u){
-      Userserv.add(u);
-      $scope.user = null;
+    $scope.addUser = function(isValid, u){
+      $scope.submitted = true;
+      console.log(isValid);
+      if(isValid){
+        console.log($scope.first);
+        
+        Userserv.add({first: $scope.first,last:$scope.last});
+        $scope.first = null;
+        $scope.last = null;
+
+      }else{
+        
+      }
+      
     };
 
     $scope.deleteUser = function(u){
